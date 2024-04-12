8eb5c32a772b6459d03ec525284a2fb2db078cdd
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2024-04-12 09:40:32 +0300

    a component to view the store item detail implemented, and used in the store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { Row, Col } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components";
+
 function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row lg={3} md={2} xs={1} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Store;
